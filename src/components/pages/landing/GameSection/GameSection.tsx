6a65251239022b3bf6bf2916b169d0cd4adc281a
alex.matos <alex.matos@ceuma.br>
2022-10-04 15:45:29 -0300

    fix: corrigindo bug de alocação dos artefatos

--- src/components/pages/landing/GameSection/GameSection.tsx
+++ src/components/pages/landing/GameSection/GameSection.tsx
@@ -1,10 +1,23 @@
-import { DragEvent } from 'react'
+import { DragEvent, useRef } from 'react'
 
 import styles from './GameSection.module.scss'
-
 export function GameSection() {
+  const tower1Ref = useRef<HTMLDivElement>(null)
+  const tower2Ref = useRef<HTMLDivElement>(null)
+  const tower3Ref = useRef<HTMLDivElement>(null)
+
   function onDragStart(e: DragEvent<HTMLSpanElement>) {
-    e.dataTransfer.setData('artifact', e.currentTarget.id)
+    const lastTowerElements = [
+      tower1Ref.current.lastChild,
+      tower2Ref.current.lastChild,
+      tower3Ref.current.lastChild
+    ]
+
+    const artifact = document.getElementById(e.currentTarget.id)
+
+    if (lastTowerElements.includes(artifact)) {
+      e.dataTransfer.setData('artifact', e.currentTarget.id)
+    }
   }
 
   function onDrop(e: DragEvent<HTMLDivElement>) {
@@ -14,8 +27,19 @@ export function GameSection() {
     const lastChild = e.currentTarget.lastChild as HTMLSpanElement
     const artifactElement = document.getElementById(artifact) as HTMLSpanElement
 
-    if (!lastChild || lastChild.clientWidth > artifactElement.clientWidth)
-      e.currentTarget.appendChild(artifactElement)
+    if (
+      !lastChild ||
+      (lastChild?.clientWidth > artifactElement?.clientWidth &&
+        !e.currentTarget.contains(artifactElement))
+    ) {
+      if (artifactElement) {
+        const artifactHeight = e.currentTarget.children.length * 20
+
+        artifactElement.style.bottom = artifactHeight + 'px'
+
+        e.currentTarget.appendChild(artifactElement)
+      }
+    }
   }
 
   function onDragOver(e: DragEvent<HTMLDivElement>) {
@@ -25,28 +49,48 @@ export function GameSection() {
   return (
     <section className={styles.container}>
       <div className={styles.towerContainer}>
-        <div className={styles.tower} onDrop={onDrop} onDragOver={onDragOver}>
+        <div
+          ref={tower1Ref}
+          className={styles.tower}
+          onDrop={onDrop}
+          onDragOver={onDragOver}
+        >
           <span
+            key="artifact3"
             id="artifact3"
             className={styles.artifact3}
             onDragStart={onDragStart}
             draggable
-          ></span>
+          />
+
           <span
+            key="artifact2"
             id="artifact2"
             className={styles.artifact2}
             onDragStart={onDragStart}
             draggable
-          ></span>
+          />
+
           <span
+            key="artifact1"
             id="artifact1"
             className={styles.artifact1}
             onDragStart={onDragStart}
             draggable
-          ></span>
+          />
         </div>
-        <div className={styles.tower} onDrop={onDrop} onDragOver={onDragOver} />
-        <div className={styles.tower} onDrop={onDrop} onDragOver={onDragOver} />
+        <div
+          ref={tower2Ref}
+          className={styles.tower}
+          onDrop={onDrop}
+          onDragOver={onDragOver}
+        />
+        <div
+          ref={tower3Ref}
+          className={styles.tower}
+          onDrop={onDrop}
+          onDragOver={onDragOver}
+        />
       </div>
 
       {/* <button className={styles.button}>Start Game</button> */}
